c98e749dfb63916de1b8a4cd20ac1cd92d105cc7
Gokhan Demirhan <demirhangokhan@gmail.com>
2021-12-23 23:18:51 +0100

    initial commit after tutorial

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,81 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import CandyMachine from "./CandyMachine";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_gokhandemirhan";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const isWalletConnected = async () => {
+    try {
+      //  Check if solana is binded to window
+      const { solana } = window;
+      if (solana) {
+        // Check wallet
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found");
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log("Public key: ", response.publicKey.toString());
+
+          // Set wallet address
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        console.log("Solana object not found");
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  // Connect wallet logic
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  // Have a button to connect wallet
+  const renderNotConnected = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect your wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await isWalletConnected();
+    };
+
+    window.addEventListener("load", onLoad);
+    return () => {
+      window.removeEventListener("load", onLoad);
+    };
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {/* Render if only not wallet connected */}
+          {!walletAddress && renderNotConnected()}
         </div>
+
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
+
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
